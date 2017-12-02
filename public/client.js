957aba3071a361321773dea022d790cf03b02b6f
itamarfiorino <itamarfiorino@gmail.com>
2017-12-02 16:17:24 -0500

    itamar's commit with colors

--- public/client.js
+++ public/client.js
@@ -1,3 +1,10 @@
+var canvas  = document.getElementById('drawing');
+var context = canvas.getContext('2d');
+context.strokeStyle = "#fff";
+function setColor(hex){
+  var styleStr = "#" + hex
+  context.strokeStyle = styleStr;
+}
 //create socket connection
 document.addEventListener("DOMContentLoaded", function() {
   var mouse = {
@@ -6,8 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
     pos: {x:0, y:0},
     pos_prev: false
   };
-  var canvas  = document.getElementById('drawing');
-  var context = canvas.getContext('2d');
+
+
   var width   = window.innerWidth;
   var height  = window.innerHeight;
   var socket = io.connect();
@@ -29,15 +36,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var line = data.line;
     context.beginPath();
     context.lineWidth = 6;
-    context.strokeStyle = '#ff0000';
     context.lineCap="round";
+    context.strokeStyle = data.color;
     context.moveTo(line[0].x * width, line[0].y * height);
     context.lineTo(line[1].x * width, line[1].y * height);
     context.stroke();
   });
   function mainLoop() {
     if (mouse.click && mouse.move && mouse.pos_prev) {
-      socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ] });
+      socket.emit('draw_line', { line: [ mouse.pos, mouse.pos_prev ], color: context.strokeStyle });
       mouse.move = false;
     }
     mouse.pos_prev = {x: mouse.pos.x, y: mouse.pos.y};
